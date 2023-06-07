7e55c72f50329c5197bc604830407493ec842a61
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-06-07 11:31:39 +0300

    Intro to OOP, properties, Classes

---
+++ 04_IntroToOOP/Program.cs
@@ -0,0 +1,186 @@
+﻿using System.Xml.Linq;
+
+namespace _04_IntroToOOP
+{
+    //private (default)
+    //protected
+    //protected internal
+    //internal 
+    //public
+    class MyClass
+    {
+       private int number;
+       private string name;
+       private const float PI = 3.14f;
+       //private readonly int id = 10;
+       private readonly int id;
+
+        public MyClass()
+        {
+            id = 10;
+
+        }
+        //void setId(int id)
+        //{
+        //    this.id = id; //error
+        //}
+        public void Print()
+        {
+            Console.WriteLine($"Id : {id} . Name : {name}");  
+        }
+        public override string ToString()
+        {
+            return $"Id : {id} . Name : {name}";  
+        }
+
+    }
+    interface ITest//Interface
+    {
+
+    }
+    class DerivedClass : MyClass, ITest//public
+    {
+
+    }
+    struct Date
+    {
+        //private int MyProperty;
+        public Date()
+        {
+
+        }
+        public Date(int a)
+        {
+            //MyProperty = a;
+        }
+    }
+    partial class Point
+    {
+        public static int count;
+        static Point()
+        {
+            count = 0;
+        }
+        public Point()
+        {
+            //number : 0
+            //boolean : false
+            //ref : null
+            count++;
+        }
+        public Point(int x, int y)
+        {
+            //SetX(x);
+            XCoord = x;
+            YCoord = y;
+            count++;
+            //SetY(y);
+        }
+        public Point(int value) : this(value, value) { }
+        public void SetX(int x)
+        {
+            if (x >= 0)
+                this.xCoord = x;
+            else
+                this.xCoord = 0;
+        }
+        public void SetY(int y)
+        {
+            if (y >= 0)
+                this.yCoord = y;
+            else
+                this.yCoord = 0;
+        }
+
+        //full property
+        private int xCoord;
+        public int XCoord
+        {
+            get
+            {
+                return xCoord;
+            }
+            set
+            {
+                if (value >= 0)
+                    this.xCoord = value;
+                else
+                    this.xCoord = 0;
+            }
+        }
+        private int yCoord;
+        public int YCoord
+        {
+            get
+            {
+                return yCoord;
+            }
+            set
+            {
+                if (value >= 0)
+                    this.yCoord = value;
+                else
+                    this.yCoord = 0;
+            }
+        }
+        //full property
+        //private string name;
+        //public string Name
+        //{
+        //    get { return name; }
+        //    set { name = value; }
+        //}
+
+        //auto propyrty
+        public string Name { get; set; }
+        public string Material { get; private set; }
+        public string Color { get; private set; } = "Green";
+        //public int ZCoord { get; set; }
+
+        //full 
+        //private int z;
+
+        //public int ZCoord
+        //{
+        //    get { return z; }
+        //    set { z = value; }
+        //}
+      
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date d = new Date();//value Date()
+            //Console.WriteLine(d.MyProperty);
+            //Console.SetCursorPosition(1, 1);
+            //Console.BackgroundColor = ConsoleColor.Green;   
+            //Console.ForegroundColor = ConsoleColor.Yellow;   
+
+            Point point = new Point(5,5);
+            point.Print();
+            Console.WriteLine(point);
+
+            point.SetX(55);
+
+            point.XCoord = 100;//set
+
+            int x = point.XCoord;//get
+            Console.WriteLine(x);
+            Console.WriteLine(point);
+
+            point.Name = "2D_Point";//set
+            Console.WriteLine(point.Name);//get
+            Point point2 = new Point(9);
+            Console.WriteLine(point2);
+
+            Console.WriteLine(Point.count);
+            /*
+            MyClass myClass = new MyClass();
+            myClass.Print();
+            Console.WriteLine(myClass);
+            Console.WriteLine(myClass.ToString());*/
+           
+        }
+    }
+}
---
+++ 05_StringBuilder/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace _05_StringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strT = "hello";
+            strT += ", world";
+
+            var b = new StringBuilder();
+
+            Console.WriteLine("Cap: " + b.Capacity);
+            b.Append(12345678901627345);
+
+            Console.WriteLine("String length: {0}", b.Length);
+            Console.WriteLine("String capacity: {0}", b.Capacity);  //memmory used
+            Console.WriteLine("Value: {0}", b.ToString());
+            Console.WriteLine();
+            Console.ReadKey();
+
+            var sb = new StringBuilder("Hello world!");
+            Console.WriteLine("String length: {0}", sb.Length);
+            Console.WriteLine("String capacity: {0}", sb.Capacity);  //memmory used
+
+            sb.Append("bla");
+            sb.Append("bla");
+            sb.Append("bla");
+            sb.AppendLine();
+            //sb.Append('\n');
+            sb.Append("bla");
+            sb.Append("bla");
+
+            Console.WriteLine("String length: {0}", sb.Length);
+            Console.WriteLine("String capacity: {0}", sb.Capacity);  //memmory used
+            Console.ReadKey();
+
+            StringBuilder sb1 = new StringBuilder(20);
+            Console.WriteLine("String length: {0}", sb1.Length);
+            Console.WriteLine("String capacity: {0}", sb1.Capacity);  //memmory used
+
+            sb1.Append("bla");
+            sb1.Append("bla");
+            sb1.Append("bla");
+            sb1.AppendLine();
+            sb1.AppendLine();
+            sb1.Append("bla");
+            sb1.Append("bla");
+            sb1.Append("bla");
+
+            Console.WriteLine("String length: {0}", sb1.Length);
+            Console.WriteLine("String capacity: {0}", sb1.Capacity);  //memmory used
+
+            string result = sb1.ToString();
+            Console.WriteLine(result);
+
+            Console.WriteLine($"MachineName: {Environment.MachineName}");
+            Console.WriteLine($"UserName: {Environment.UserName}");
+            Console.WriteLine($"OSVersion: {Environment.OSVersion}");
+            Console.WriteLine($"CommandLine: {Environment.CommandLine}");
+            Console.WriteLine($"Desktop Path: {Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}");
+
+            Console.ReadKey();
+        }
+    }
+}
